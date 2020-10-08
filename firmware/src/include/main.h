bed3ffbd6008fed21378ed9b69dd5e83360c6a00
Alija Bobija <abobijax@gmail.com>
2020-10-08 22:47:43 +0200

    Only 8MHz oscillator is supported

--- firmware/src/include/main.h
+++ firmware/src/include/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#if(__FOSC__ != 8000 && __FOSC__ != 4000 && __FOSC__ != 2000)
+/*#if(__FOSC__ != 8000 && __FOSC__ != 4000 && __FOSC__ != 2000)
     #error Choose another oscillator frequency. Supported ones are: 8MHz, 4MHz and 2MHz.
+#endif*/
+
+#if(__FOSC__ != 8000)
+    #error Only 8MHz oscillator is supported.
 #endif
 
 #include "defs.h"
